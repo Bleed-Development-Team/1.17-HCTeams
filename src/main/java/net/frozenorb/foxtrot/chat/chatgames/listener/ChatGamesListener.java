e879787a2386bad72782dd8b6c2cd813a565e558
Nopox-Minecraft <78834703+Nopox-Minecraft@users.noreply.github.com>
2022-04-22 13:49:46 -0700

    Hopefully fixed chat games

--- src/main/java/net/frozenorb/foxtrot/chat/chatgames/listener/ChatGamesListener.java
+++ src/main/java/net/frozenorb/foxtrot/chat/chatgames/listener/ChatGamesListener.java
@@ -1,6 +1,7 @@
 package net.frozenorb.foxtrot.chat.chatgames.listener;
 
 import net.frozenorb.foxtrot.Foxtrot;
+import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
@@ -13,8 +14,9 @@ public class ChatGamesListener implements Listener {
         if (Foxtrot.getInstance().getChatGamesHandler().isGoingOn()){
             if (Foxtrot.getInstance().getChatGamesHandler().isCorrect(event.getMessage())){
                 event.setCancelled(true);
-
-                Foxtrot.getInstance().getChatGamesHandler().endGame(false, event.getPlayer());
+                Bukkit.getScheduler().runTask(Foxtrot.getInstance(), () -> {
+                    Foxtrot.getInstance().getChatGamesHandler().endGame(false, event.getPlayer());
+                        });
             }
         }
     }
