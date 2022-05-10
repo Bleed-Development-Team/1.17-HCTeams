444018f03d8e0370ad6b85ff80dfa9b4284f397c
Nopox-Minecraft <78834703+Nopox-Minecraft@users.noreply.github.com>
2022-05-09 21:02:11 -0700

    Added hellforged

--- src/main/java/net/frozenorb/foxtrot/extras/enchants/CustomEnchant.java
+++ src/main/java/net/frozenorb/foxtrot/extras/enchants/CustomEnchant.java
@@ -2,27 +2,34 @@ package net.frozenorb.foxtrot.extras.enchants;
 
 import net.frozenorb.foxtrot.Foxtrot;
 import net.frozenorb.foxtrot.extras.enchants.listeners.FireResistanceEnchant;
+import net.frozenorb.foxtrot.extras.enchants.listeners.HellForgedEnchant;
 import net.frozenorb.foxtrot.extras.enchants.listeners.InvisibilityEnchant;
 import net.frozenorb.foxtrot.extras.enchants.listeners.SpeedEnchant;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 
-public class CustomEnchant {
+public class CustomEnchant implements Listener {
 
     public static void init() {
         Bukkit.getLogger().info("Custom Enchantments are enabled!");
         Foxtrot.getInstance().getServer().getPluginManager().registerEvents(new FireResistanceEnchant(), Foxtrot.getInstance());
         Foxtrot.getInstance().getServer().getPluginManager().registerEvents(new SpeedEnchant(), Foxtrot.getInstance());
         Foxtrot.getInstance().getServer().getPluginManager().registerEvents(new InvisibilityEnchant(), Foxtrot.getInstance());
+        Foxtrot.getInstance().getServer().getPluginManager().registerEvents(new HellForgedEnchant(), Foxtrot.getInstance());
 
-        Foxtrot.getInstance().getServer().getScheduler().runTaskTimer(Foxtrot.getInstance(), () -> {
-            Bukkit.getOnlinePlayers().forEach(CustomEnchant::refreshEnchants);
-        }, 20L, 5L);
     }
 
     private static void refreshEnchants(Player player) {
         FireResistanceEnchant.refreshFireResistance(player);
         SpeedEnchant.refreshSpeed(player);
         InvisibilityEnchant.refreshInvisibility(player);
+    }
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event) {
+        refreshEnchants(event.getPlayer());
     }
 }
---
+++ src/main/java/net/frozenorb/foxtrot/extras/enchants/listeners/HellForgedEnchant.java
@@ -0,0 +1,24 @@
+package net.frozenorb.foxtrot.extras.enchants.listeners;
+
+import net.frozenorb.foxtrot.extras.enchants.events.PlayerArmorEquipEvent;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.Damageable;
+
+public class HellForgedEnchant implements Listener {
+
+    @EventHandler
+    public void onMove(PlayerMoveEvent event) {
+        for (ItemStack item : event.getPlayer().getInventory().getArmorContents()) {
+            if (!item.getItemMeta().hasLore()) continue;
+            if (!item.getItemMeta().getLore().contains("&4HellForged IV")) continue;
+
+            Damageable damageable = (Damageable) item.getItemMeta();
+
+            damageable.setDamage(damageable.getDamage() + 5);
+
+        }
+    }
+}
